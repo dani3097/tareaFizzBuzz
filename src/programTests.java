b5693c728152301b172973a9ad7a3c1fa615895d
daniela vela <danielitavo97@gmail.com>
2020-02-11 10:03:26 -0400

    test fizzBuzz

--- src/programTests.java
+++ src/programTests.java
@@ -8,5 +8,20 @@ class programTests {
 	void testUno() {
 		assertEquals(FizzBuzz.fizzBuzz(1),"1");
 	}
-
+	@Test
+	void testDos() {
+		assertEquals(FizzBuzz.fizzBuzz(2),"2");
+	}
+	@Test
+	void testTres() {
+		assertEquals(FizzBuzz.fizzBuzz(3),"Fizz");
+	}
+	@Test
+	void testCinco() {
+		assertEquals(FizzBuzz.fizzBuzz(5),"Buzz");
+	}
+	@Test
+	void testQuince() {
+		assertEquals(FizzBuzz.fizzBuzz(15),"FizzBuzz");
+	}
 }
